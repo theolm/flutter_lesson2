a0bb56c51e525ef971e2d59ca0a421c9e471c1e8
Theo <theolm.mota@gmail.com>
2020-01-21 22:27:35 -0300

    pdf and TODO list

---
+++ aula_2/lib/taskrow.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TaskRow extends StatelessWidget {
+  final String text;
+  final Function callback;
+
+  const TaskRow({Key key, @required this.text, @required this.callback}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(text),
+      trailing: IconButton(
+        onPressed: () {
+          delete();
+        },
+        icon: Icon(Icons.delete),
+      ),
+    );
+  }
+
+  void delete() async {
+    var prefs = await SharedPreferences.getInstance();
+    List<String> tasks = prefs.getStringList('tasks');
+    tasks.remove(text);
+    prefs.setStringList('tasks', tasks);
+    callback();
+  }
+}
